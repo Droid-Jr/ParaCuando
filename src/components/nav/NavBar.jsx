ddd93dd74be31aecde4df81c7c9721ad07f8dba1
joseph <droidjr10@gmail.com>
2023-11-18 16:39:30 -0500

    xd

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <nav className='w-[100%] h-[100px] bg-[red] flex justify-around items-center'>
        <h2>PC?</h2>
        <div className='flex gap-6'>
-        <Link to="/register">REGISTER</Link>
+        <Link to="/register">REGIST</Link>
         <Link to="/">Homes</Link>
        </div>
     </nav>
