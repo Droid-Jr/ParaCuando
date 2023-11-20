d72a434eed2ad21b79806361ab23d2dd7900e67f
Thecomandate <richard26.fm@gmail.com>
2023-11-20 15:07:43 -0500

    hola

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -5,11 +5,11 @@ function NavBar() {
     <nav className='flex justify-around items-center'>
        <h2>PC?</h2>
        <div className='flex gap-6'>
-        <Link to="/register">REGIST</Link>
+        <Link to="/register">REGISTro richard</Link>
         <Link to="/">Homes</Link>
        </div>
     </nav>
   )
 }
 
-export default NavBar
+export default NavBarg
