a6252b5cc1dddd3e2821ca039a4fcd5345be811a
Prince Moreno <prins_ghost_2018@outlook.com>
2023-11-20 19:21:21 -0500

    Tus cambios descriptivos

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Fields } from "./Login";
 import { useForm } from "react-hook-form";
 import axios from "axios";
@@ -13,45 +12,13 @@ function Register({ setUser }) {
       })
       .catch((err) => console.log(err));
   };
-=======
-// import { Fields } from "./Login";
-import axios from "axios";
-import { Fields } from "../pages/Login";
-import { Toaster, toast } from "sonner";
-import { useForm } from "react-hook-form"
-import { useNavigate } from "react-router-dom";
-
-function Register() {
-  const navigate = useNavigate()
-  const {register , handleSubmit} = useForm()
-  const registers = (data) => {
-    axios.post(`http://localhost:4000/register`, data)
-      .then(res => {
-        console.log(res.data);
-        toast.success("Registro Exitoso");
-       setTimeout(() => {
-        navigate("/login");
-       }, 1200);
-      })
-      .catch(err => {
-        console.error("Error:", err);
-      });
-  };
-  
-  
->>>>>>> 2d65d3802abab807c4501f254cf9ae9d746cae6e
 
   return (
     <div className="h-full text-white font-semibold px-4 flex flex-col items-center gap-8 py-8 lg:flex-row sm:gap-12 sm:px-12 sm:py-12 sm:justify-center lg:px-24 md:py-0">
       <Fields />
       <form
-<<<<<<< HEAD
         onSubmit={handleSubmit(registerNew)}
         className="relative h-full border w-full rounded-xl flex flex-col justify-center gap-2 px-12 bg-opacity-70 bg-black lg:flex-1 sm:h-4/5 lg:gap-4 md:gap-4"
-=======
-         onSubmit={handleSubmit(registers)}
-        className="relative h-full border w-full rounded-xl flex flex-col justify-center gap-2 px-12 bg-opacity-70 bg-black lg:flex-1 sm:h-4/5 lg:gap-2 md:gap-4 2xl:gap-4"
->>>>>>> 2d65d3802abab807c4501f254cf9ae9d746cae6e
       >
         <img
           src="../../public/imgs/login/Group 296.svg"
@@ -74,7 +41,6 @@ function Register() {
         />
         <span className="flex gap-2">
           <span className="w-full">
-<<<<<<< HEAD
             <label htmlFor="username" className="font-bold">
               Nombre
             </label>
@@ -83,20 +49,11 @@ function Register() {
               {...register("username")}
               
               id="username"
-=======
-            <label htmlFor="username" className="font-bold">Nombre</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              {...register("username")}
->>>>>>> 2d65d3802abab807c4501f254cf9ae9d746cae6e
               placeholder="John Doe"
               className="p-3 rounded-md bg-transparent border-2 w-full"
             />
           </span>
           <span className="w-full">
-<<<<<<< HEAD
             <label htmlFor="lastname" className="font-bold">
               Apellido
             </label>
@@ -105,14 +62,6 @@ function Register() {
           
               {...register("lastname")}
               id="lastname"
-=======
-            <label htmlFor="lastname" className="font-bold">Apellido</label>
-            <input
-              type="text"
-              name="lastname"
-              id="lastname"
-              {...register("lastname")}
->>>>>>> 2d65d3802abab807c4501f254cf9ae9d746cae6e
               placeholder="John Doe"
               className="p-3 rounded-md bg-transparent border-2 w-full"
             />
@@ -132,16 +81,9 @@ function Register() {
         <li className="text-xs">
           Las Contraseñas deben tener mayusculas, minusculas y numeros
         </li>
-<<<<<<< HEAD
         <button className="p-3 bg-[#F3F243] rounded-md text-black font-bold my-4">
           Crear Cuenta
         </button>
-=======
-        <button
-          type="submit"
-          className="p-3 bg-[#F3F243] rounded-md text-black font-bold my-4"
-        >Rregister </button>
->>>>>>> 2d65d3802abab807c4501f254cf9ae9d746cae6e
         <a href="" className="text-center text-[#F3F243] underline">
           O inicia sesion
         </a>
