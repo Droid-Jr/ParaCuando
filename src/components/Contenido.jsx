a5481b48cf4da4a1aa210cf44c1c440ceeba4342
juanC <jcct51066@gmail.com>
2023-11-28 16:55:03 -0400

    avnace ya con loop true y mas entendible

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -6,10 +6,68 @@ import { Navigation } from 'swiper/modules';
 
 export default function Contenido_Populares() {
   return (
-    <div className="w-full px-4 sm:px-8 md:px-16 py-8 ">
-      <h1 className="text-BLACK-LIGHT font-roboto text-[24px] font-medium">Populares en Bolivia</h1>
-      <p className="font-roboto font-normal text-[16px] leading-6 text-graydark w-[222px]">Los mejores Hoteles de Bolivia</p>
+    <div className="w-full px-4 sm:px-8 md:px-16  ">
+      <h1 className="text-BLACK-LIGHT font-roboto text-[24px] font-medium mt-[10px]">Populares en Bolivia</h1>
+      
 
+      <p className="font-roboto font-normal text-[16px] leading-6 text-graydark w-    [222px] mt-[-8px]">Los mejores Hoteles de Bolivia</p>
+
+      <div className="mb-[100px] mt-[50px] w-[964px] h-[464px]">
+      
+
+      <Swiper navigation={true} loop={true}  modules={[Navigation]} className="grid gap-4 " slidesPerView={1.3} spaceBetween={15} breakpoints={{
+        640: {
+          slidesPerView: 1.3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}>
+        {json.map((valor, index) => (
+          <SwiperSlide key={index}>
+
+            <div className="w-full sm:w-[299px] h-[454px] border-2 rounded-2xl ">
+              
+              <img src={valor.img} alt="" className="h-[239px] sm:w-[299px] w-full object-cover rounded-tl-2xl rounded-tr-2xl overflow-hidden " />
+
+              <div className="p-6">
+                  <div className="mt-[-18px]"><p className="font-roboto font-normal text-[20px] text-black text-left ">{valor.title}</p>
+                  
+
+                    <p className="mt-[-2px] text-[15px]">{valor.datos}</p>
+
+                    <p className="mt-[60px] text-blue-500"><a href="#">{valor.url}</a></p>
+
+                  </div>
+
+                  <div className="flex items-center mt-[20px]"> {/*div para img y texto del logo de votos*/}
+                    <img src={valor.logo} alt="" className="w-[15.33px] h-[15.33px]  "/>
+                    <p className="px-1 text-black font-bold text-[14px]" >{valor.votos}</p>
+                    
+                  </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          
+        ))}
+      </Swiper>
+      </div>
+
+
+
+      {/*segundo slides*/}
+
+{/* 
+      <div className=" mt-[50px] mb-[30px]">
+      <h1 className="text-BLACK-LIGHT font-roboto text-[24px] font-medium mt-[10px]">Sugerencias en Para Ti</h1>
+
+      <p className="font-roboto font-normal text-[16px] leading-6 text-graydark w-    [222px] mt-[-8px]">Publicaciones que podrias apoyar</p>
       <Swiper navigation={true} modules={[Navigation]} className="grid gap-4 " slidesPerView={1.3} spaceBetween={15} breakpoints={{
         640: {
           slidesPerView: 1.3,
@@ -26,28 +84,32 @@ export default function Contenido_Populares() {
       }}>
         {json.map((valor, index) => (
           <SwiperSlide key={index}>
+
             <div className="w-full sm:w-[299px] h-[454px] border-2 rounded-2xl ">
               
-              <img src={valor.img} alt="" className="h-[239px] sm:h-[299px] w-full object-cover rounded-tl-2xl rounded-tr-2xl overflow-hidden " />
+              <img src={valor.img} alt="" className="h-[239px] sm:w-[299px] w-full object-cover rounded-tl-2xl rounded-tr-2xl overflow-hidden " />
 
               <div className="p-6">
                   <div className="mt-[-18px]"><p className="font-roboto font-normal text-[20px] text-black text-left ">{valor.title}</p>
 
-                    <p className="mt-[-6px] text-[15px]">{valor.datos}</p>
+                    <p className="mt-[-2px] text-[15px]">{valor.datos}</p>
 
-                    <p className="mt-[15px] text-blue-500"><a href="#">{valor.url}</a></p>
+                    <p className="mt-[60px] text-blue-500"><a href="#">{valor.url}</a></p>
 
                   </div>
 
-                  <div className="flex items-center mt-[8px]">
+                  <div className="flex items-center mt-[20px]">
                     <img src={valor.logo} alt="" className="w-[15.33px] h-[15.33px]  "/>
                     <p className="px-1 text-black font-bold text-[14px]" >{valor.votos}</p>
                   </div>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+          
         ))}
       </Swiper>
+
+      </div>*/}
     </div>
   );
 }
